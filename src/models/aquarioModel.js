df868c9f11f1e0ec3e9853174203bfe02c725b75
Kaue-Tamura27 <kaue.gomes@sptech.school>
2026-05-07 13:58:25 -0300

    Integração com o BD local e iniciando o quiz

--- src/models/aquarioModel.js
+++ src/models/aquarioModel.js
@@ -1,12 +1,12 @@
 var database = require("../database/config");
 
-function buscarAquariosPorEmpresa(empresaId) {
+/*function buscarAquariosPorEmpresa(empresaId) {
 
   var instrucaoSql = `SELECT * FROM aquario a WHERE fk_empresa = ${empresaId}`;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
-}
+}*/
 
 function cadastrar(empresaId, descricao) {
   
